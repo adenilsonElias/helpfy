713835a2724fba9b00a89503f012945030cf4546
gc <gabrielciccotti1@gmail.com>
2020-08-05 16:28:56 -0300

    toolbar

--- src/screens/Profile/Profile.js
+++ src/screens/Profile/Profile.js
@@ -7,7 +7,9 @@ import {
 import AuthContext from '../../context/auth_context'
 import Login from '../Login/Login';
 import { StackLogin } from '../../routes/StackLogin';
-import Top from './components/BackgroundTop/BackgroundTop'
+import BackgroundTop from './components/BackgroundTop/BackgroundTop'
+import ToolbarMid from './components/ToolbarMid/ToolbarMid'
+import ProfileInfoBot from './components/ProfileInfoBot/ProfileInfoBot'
 
 export default Profile = () => {
     // useContext 
@@ -16,6 +18,8 @@ export default Profile = () => {
         return (
             <View>
                 <BackgroundTop />
+                <ToolbarMid />
+                <ProfileInfoBot title={'Nome'} item={this.props.name} icon={'user'}/>
                 <Button title="logout" onPress={() => auth.logOut()}></Button>
             </View>
         )
