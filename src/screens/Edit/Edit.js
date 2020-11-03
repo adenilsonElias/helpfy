388a106d9c801aa788f2fc0d5129d02187380f64
Lukazukimo <fabio.wuhuang@hotmail.com>
2020-11-03 01:23:46 -0300

    Add Picker State on Edit Profile

--- src/screens/Edit/Edit.js
+++ src/screens/Edit/Edit.js
@@ -4,7 +4,7 @@ import AuthContext from '../../context/auth_context'
 import { useNavigation, useRoute } from '@react-navigation/native'
 import Icon from 'react-native-vector-icons/Feather'
 import Icon2 from 'react-native-vector-icons/MaterialCommunityIcons'
-import style from './style'
+import style, { placeHolderStyle, placeholderValue, placeholderValue2 } from './style'
 import User from '../../model/user'
 import { CreateNewUser } from '../../firebase/Authentication'
 import { color1 } from '../../global/constant/constant'
@@ -14,11 +14,16 @@ import EditImage from './components/Edit_Image/Edit_Image'
 import EditBackground from './components/Edit_Background/Edit_Background'
 import Header from './components/Header/Header'
 import TheDatePicker from './components/TheDatePicker/TheDatePicker'
+import RNPickerSelect from 'react-native-picker-select';
+import { getCities, getStates } from '../../api/ibge'
 
 export default Edit = () => {
 	const navigation = useNavigation()
 	const usesrParam: User = useRoute().params.user;
 	const auth = useContext(AuthContext);
+	const [stateUF, setStateUf] = useState('')
+	const [cityList, setCityList] = useState([])
+	const [stateList, setStateList] = useState([])
 	const [email, setEmail] = useState(usesrParam ? usesrParam.email : '')
 	const [name, setName] = useState(usesrParam ? usesrParam.name : '')
 	const [birth, setBirth] = useState(usesrParam ? usesrParam.birthDay : '')
@@ -52,7 +57,49 @@ export default Edit = () => {
 				</TouchableOpacity>
 			)
         })
-    }, [])
+	}, [])
+
+	useEffect(() => {
+		async function getStatesFunction() {
+			setStateList(await getStates())
+		}
+		getStatesFunction()
+	}, [])
+
+	async function getCitiesFunction(sigla: String) {
+		setCityList(await getCities(sigla))
+	}
+	
+	const renderCity = () => {
+		if (stateUF == '') {
+			return(
+				<View />
+			)
+		} else {
+			return(
+				<RNPickerSelect
+						value={stateUF != '' ? city : ''}
+						onValueChange={(value, index) => {
+							if (value != '') {
+								setCity(value)
+							}
+							else {
+								setCity('')
+							}
+						}}
+						items={cityList.map((city) => {
+							return { label: city, value: city }
+						})}
+						placeholder={placeholderValue2}
+						style={placeHolderStyle}
+						useNativeAndroidPickerStyle={false}
+						Icon={() => {
+							return <Icon name={'map-pin'} size={26} color={color1} style={style.pickerIcon} />
+						}}
+					/>
+			)
+		}
+	}
 
     return (
 		<>
@@ -83,7 +130,39 @@ export default Edit = () => {
 						onChangeText={email => setEmail(email)} />
 				</View>
 				<TheDatePicker birth={birth} setBirth={setBirth}/>
-				<View style={style.inputContainer}>
+				<RNPickerSelect
+					onValueChange={(value, index) => {
+						setStateUf('')
+						if (value != '') {
+							setStateUf(value)
+							setState(stateList[index - 1].nome);
+							getCitiesFunction(value)
+						}
+						else {
+							setStateUf('')
+							setState('')
+							setCity('')
+							setCityList([])
+						}
+					}}
+					items={stateList.map((e) => {
+						return { label: e.nome, value: e.sigla }
+					})}
+					placeholder={placeholderValue}
+					// placeholder={{
+					// 	label: 'Selecione um estado',
+					// 	value: '',
+					// }}
+					style={placeHolderStyle}
+					useNativeAndroidPickerStyle={false}
+					Icon={() => {
+						return(
+							<Icon name={'map-pin'} size={26} color={color1} style={style.pickerIcon} />
+						)
+					}}
+				/>
+				{ renderCity() }
+				{/* <View style={style.inputContainer}>
 					<Icon name={'map-pin'} size={26} color={color1} style={style.icon} />
 					<TextInput style={style.input}
 						placeholder='Estado'
@@ -102,7 +181,7 @@ export default Edit = () => {
 						value={city}
 						underlineColorAndroid='transparent'
 						onChangeText={city => setCity(city)} />
-				</View>
+				</View> */}
 				<View style={style.inputContainer}>
 					<Icon name={'lock'} size={26} color={color1} style={style.icon} />
 					<TouchableOpacity style={[style.input, { justifyContent: 'center' }]}
