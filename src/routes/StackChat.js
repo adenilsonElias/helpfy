8e98d8e34bdcb44280abc2193848c0b919c728aa
Lukazukimo <fabio.wuhuang@hotmail.com>
2020-11-28 03:05:19 -0300

    Correcao Bug voltar tab bar Bloqueio Chat

--- src/routes/StackChat.js
+++ src/routes/StackChat.js
@@ -1,32 +1,32 @@
-import React, { useEffect } from 'react'
+import React, { useCallback } from 'react'
 import { View, Text } from 'react-native'
 import { createStackNavigator } from '@react-navigation/stack';
 import Chat_List from '../screens/Chat_List/Chat_List'
 import TheChat from '../screens/The_Chat/TheChat'
 import { color1, color2, styleTitle } from '../global/constant/constant';
-import { useNavigation } from '@react-navigation/native';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
 import { useSelector } from 'react-redux';
 
 const Stack = createStackNavigator();
 
-export function StackChat () {
+export function StackChat() {
     const navigation = useNavigation()
     const user = useSelector(state => state.userState.user)
 
-    useEffect (() => {
-        if (user == null){
+    useFocusEffect(useCallback(() => {
+        if (user == null) {
             navigation.navigate('Profile')
-            navigation.addListener('tabPress', e => {                
+            navigation.addListener('tabPress', e => {
                 e.preventDefault();
                 navigation.navigate('Profile')
             })
         }
-    })
+    }, []))
 
-    return(
+    return (
         <Stack.Navigator>
             <Stack.Screen name="ChatList" component={Chat_List} options={{ headerShown: false }} />
-            <Stack.Screen name="TheChat" component={TheChat} 
+            <Stack.Screen name="TheChat" component={TheChat}
                 options={{
                     headerStyle: {
                         backgroundColor: color2,
